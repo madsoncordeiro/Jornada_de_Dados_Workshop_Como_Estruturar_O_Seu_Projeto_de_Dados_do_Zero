e757737175a97df29aaf20076e026f2a9fe2ad60
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-04-21 09:26:22 -0300

    Adicionando etapa Extract do ETL

---
+++ app/pipeline/extract.py
@@ -0,0 +1,31 @@
+import os # Biblioteca para manipular arquivos e pastas
+import glob # Biblioteca para listar arquivos
+
+import pandas as pd # Biblioteca para manipulação de dados
+
+from typing import List # Biblioteca para tipagem de listas
+
+"""
+Função para ler os arquivos de uma pasta data/input e retornar uma lista de DataFrames
+
+args: input_path (str): caminho da pasta com os arquivos
+
+return: lista de DataFrames
+"""
+
+path = "data/input"
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+    
+    data_frame_list = []
+    
+    for file in all_files:
+        data_frame_list.append(pd.read_excel(file))
+        
+    return data_frame_list
+
+# Criando uma variável que vai comportar todas essas listas, está chamando a função criada e passando o path
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel(path)
+    print(data_frame_list)
